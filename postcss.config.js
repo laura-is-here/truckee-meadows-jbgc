b833765134c517b07917d1e03cb908e866d298f3
laura <lauras_here@pm.me>
2020-03-21 16:45:30 -0700

    Enable purgecss

--- postcss.config.js
+++ postcss.config.js
@@ -14,6 +14,6 @@ module.exports = {
   plugins: [
     require("tailwindcss"),
     require("autoprefixer"),
-    ...(process.env.NODE_ENV === "production" ? [purgecss] : [])
+    ...(process.env.NODE_ENV !== "production" ? [purgecss] : [])
   ]
 };
