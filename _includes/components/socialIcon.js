d593efe7de6a94e7788128eb90ed15c63dab023d
gibbet-grate <62523656+gibbet-grate@users.noreply.github.com>
2020-03-24 10:40:52 -0700

    Update socialIcon.js

--- _includes/components/socialIcon.js
+++ _includes/components/socialIcon.js
@@ -1,10 +1,8 @@
 module.exports = (content, icon, link) => {
   return `
 <p>
-<span class="iconify inline-block" data-icon="fa:${icon}"></span>
-<a href="${link}" rel="noopener noreferer" target="_blank">
-${content}
-</a>
+<span class="iconify inline-block" data-icon="fa:${icon}"></span>&nbsp;<a href="${link}" rel="noopener noreferer" target="_blank">
+${content}</a>
 </p>
 `;
 };
