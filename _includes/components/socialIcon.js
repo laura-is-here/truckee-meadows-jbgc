954c4b34cb90c5337c55441dd4cf5f9b977e7050
laura <lauras_here@pm.me>
2020-03-24 10:30:24 -0700

    Update social icon shortcode

--- _includes/components/socialIcon.js
+++ _includes/components/socialIcon.js
@@ -1,8 +1,8 @@
 module.exports = (content, icon, link) => {
   return `
 <p>
-<a href="${link}" rel="noopener noreferer" target="_blank">
 <span class="iconify inline-block" data-icon="fa:${icon}"></span>
+<a href="${link}" rel="noopener noreferer" target="_blank">
 ${content}
 </a>
 </p>
