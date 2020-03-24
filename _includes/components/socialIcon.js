7afcba86e1ce5dc81b3657da6e4624729235ad5d
laura <lauras_here@pm.me>
2020-03-24 10:27:02 -0700

    Fix social icon shortcode

--- _includes/components/socialIcon.js
+++ _includes/components/socialIcon.js
@@ -1,7 +1,7 @@
 module.exports = (content, icon, link) => {
   return `
 <p>
-<a href="${link} rel="noopener noreferer" target="_blank">
+<a href="${link}" rel="noopener noreferer" target="_blank">
 <span class="iconify inline-block" data-icon="fa:${icon}"></span>
 ${content}
 </a>
