7ad50565eb6a8a67e084570e5e068a60860b7ec0
RGB Pregmatch <reno.tmjbgc@protonmail.com>
2020-04-05 10:44:08 -0700

    Update socialIcon.js

--- _includes/components/socialIcon.js
+++ _includes/components/socialIcon.js
@@ -1,10 +1,9 @@
 module.exports = (content, icon, link) => {
   return `
-<div style="clear:all;">
-<span class="iconify inline-block" data-icon="fa:${icon}"></span>
-<a href="${link}" rel="noopener noreferer" target="_blank">
-${content}
-</a>
+<div>
+	<span class="iconify inline-block" data-icon="fa:${icon}"></span>
+	<a href="${link}" rel="noopener noreferer" target="_blank">${content}</a>
 </div>
+<br clear="all" />
 `;
 };
